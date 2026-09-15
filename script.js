6392ef0840b702581100cb3f76825dd093e4e3bc
heber <heberhuayta08@gmail.com>
2026-09-14 23:20:16 -0300

    WIP: WebRTC implementation, receiver still needs fixes

--- script.js
+++ script.js
@@ -1,19 +1,75 @@
 const button = document.getElementById('shareButton')
 const video = document.getElementById('screen')
 
+const videoRemote = document.getElementById('remote')
+const buttonRemote = document.getElementById('reciveRemote')
+
+const answerInput = document.getElementById('answerInput')
+const buttonResponseInput = document.getElementById('responseInput')
+
+const offerInput = document.getElementById('offerInput')
+
+let peer
+
 button.addEventListener("click", async() => {
 
     try{
-
+        
         const stream = await navigator.mediaDevices.getDisplayMedia({
 
             video: true,
             audio: true
         })
 
-        video.srcObject = stream
+        peer = new RTCPeerConnection();
+
+        stream.getTracks().forEach(track => {
+
+            peer.addTrack(track, stream)
+        })
+        
+        const offer = await peer.createOffer()
+
+        await peer.setLocalDescription(offer)
+
+        console.log(offer)
 
     }catch(error){
+
+        console.error(error)
+    }
+})
+
+buttonResponseInput.addEventListener("click", async() =>{
+
+    const answerRemote = JSON.parse(answerInput.value)
+
+     await peer.setRemoteDescription(answerRemote)
+})
+
+
+buttonRemote.addEventListener("click", async() => {
+
+    try{
+
+        // peer = new RTCPeerConnection();
+
+        // peer.ontrack = (event) => {
+
+        //     videoRemote.srcObject = event.streams[0]
+        // }
+
+        // const offer = JSON.parse(offerInput.value)
+
+        // await peer.setRemoteDescription(offer)
+
+        // const answer = await peer.createAnswer()
+
+        // await peer.setLocalDescription(answer)
+
+        console.log("hola")
+
+    } catch(error){
 
         console.error(error)
     }
